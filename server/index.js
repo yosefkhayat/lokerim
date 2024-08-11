893a9d61951c8b87c250ca3120dbd052873d84b5
yosef khayat <yousef.khayat24@gmail.com>
2024-08-12 01:13:18 +0300

    add get questions

--- server/index.js
+++ server/index.js
@@ -7,16 +7,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 (async () => {
-// Parse the credentials JSON from the environment variable
-const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+  // Parse the credentials JSON from the environment variable
+  const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
-const auth = new google.auth.GoogleAuth({
-  credentials: {
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
       client_email: credentials.client_email,
       private_key: credentials.private_key.replace(/\\n/g, '\n'),
-  },
-  scopes: "https://www.googleapis.com/auth/spreadsheets",
-});
+    },
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
 
   // Create client instance for auth
   const client = await auth.getClient();
@@ -27,26 +27,33 @@ const auth = new google.auth.GoogleAuth({
   const spreadsheetId = "19AviszQbiPmqZHoRnPUHI3TJJimtYYLZzpnH_VmQcdI";
 
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello, world!");
-});
-
-app.post("/", async (req, res) => {
-  const { username, score } = req.body;
-  
-  // Write row(s) to spreadsheet
-  await googleSheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId,
-    range: "info!A:B",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values: [[username, score]],
-    },
+  app.get("/", (req, res) => {
+    res.status(200).send("Hello, world!");
   });
+  app.get("/question", async(req, res) => {
+    const getRows = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: "questions!A:G",
+    });
+    res.status(200).send(getRows.data.values);
+  });
+  app.post("/", async (req, res) => {
+    const { username, score } = req.body;
 
-  res.send("Successfully submitted! Thank you!");
-});
+    // Write row(s) to spreadsheet
+    await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: "info!A:B",
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [[username, score]],
+      },
+    });
+
+    res.send("Successfully submitted! Thank you!");
+  });
 })();
-const port =  process.env.PORT || 1337; 
+const port = process.env.PORT || 1337;
 app.listen(port, (req, res) => console.log("running on 1337"));
