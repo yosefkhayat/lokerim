9a170a43082d97e94a3eaaf9818deb465b0e1ec3
yosef khayat <yousef.khayat24@gmail.com>
2024-08-12 00:58:49 +0300

    update async call

--- server/index.js
+++ server/index.js
@@ -6,17 +6,17 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
+(async () => {
 // Parse the credentials JSON from the environment variable
-  const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
+const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
 
-  const auth = new google.auth.GoogleAuth({
-    credentials: {
-        client_email: credentials.client_email,
-        private_key: credentials.private_key.replace(/\\n/g, '\n'),
-    },
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+      client_email: credentials.client_email,
+      private_key: credentials.private_key.replace(/\\n/g, '\n'),
+  },
+  scopes: "https://www.googleapis.com/auth/spreadsheets",
+});
 
   // Create client instance for auth
   const client = await auth.getClient();
@@ -47,5 +47,6 @@ app.post("/", async (req, res) => {
 
   res.send("Successfully submitted! Thank you!");
 });
+})();
 const port =  process.env.PORT || 1337; 
 app.listen(port, (req, res) => console.log("running on 1337"));
